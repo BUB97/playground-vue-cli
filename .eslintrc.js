658c7ea649aedeeeb136fdc5ce306642a37b1fb5
BUB97 <shuhang_zhan@sina.com>
2023-10-26 12:47:54 +0800

    feat: 配置eslint以及errorFix

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+    tsconfigRootDir: __dirname,
+  },
+  extends: [
+    "eslint:recommended",
+    "prettier",
+    "@tencent/eslint-config-tencent",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  plugins: ["@typescript-eslint"],
+  env: {
+    es6: true,
+    node: true,
+  },
+  rules: {
+    // 启用额外规则
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+
+    // override configuration set by extending "eslint:recommended"
+    "no-empty": "warn",
+    "no-cond-assign": ["error", "always"],
+
+    // 禁用基础配置汇总的规则
+    "for-direction": "off",
+  },
+  ignorePatterns: ["dist", "node_modules"],
+};
